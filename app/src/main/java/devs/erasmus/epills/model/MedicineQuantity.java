443faebb8fefd79b67e272cf66bb22a465637315
Jose Collado <jocolsan@inf.upv.es>
2017-10-29 01:12:29 +0000

    models initialized with some main atributes

--- app/src/main/java/devs/erasmus/epills/model/MedicineQuantity.java
+++ app/src/main/java/devs/erasmus/epills/model/MedicineQuantity.java
@@ -5,4 +5,37 @@ package devs.erasmus.epills.model;
  */
 
 public class MedicineQuantity {
+    private int id;
+    private IntakeMoment intakeMoment;
+    private Medicine medicine;
+
+    public MedicineQuantity(int id, IntakeMoment intakeMoment, Medicine medicine) {
+        this.id = id;
+        this.intakeMoment = intakeMoment;
+        this.medicine = medicine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public IntakeMoment getIntakeMoment() {
+        return intakeMoment;
+    }
+
+    public void setIntakeMoment(IntakeMoment intakeMoment) {
+        this.intakeMoment = intakeMoment;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
+    }
 }
