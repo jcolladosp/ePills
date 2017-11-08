c2fde4a2a5e86071e2a41f8e6f4d849a7e152f4a
Jose Collado <jocolsan@inf.upv.es>
2017-11-08 12:25:53 +0000

    Menu clock screen added

--- app/src/main/java/devs/erasmus/epills/controller/ClockActivity.java
+++ app/src/main/java/devs/erasmus/epills/controller/ClockActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
@@ -88,6 +90,26 @@ public class ClockActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         drawer.openDrawer();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_clock, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_sync_calendar) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
