74ecb482f7e59e7d6bba7a10092b398f49694e1c
Remo Andreoli <andreoliremo@gmail.com>
2017-11-21 18:57:00 +0000

    -added BootReceiver

---
+++ app/src/main/java/devs/erasmus/epills/broadcast_receiver/BootReceiver.java
@@ -0,0 +1,16 @@
+package devs.erasmus.epills.broadcast_receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            // Set the alarm here.
+        }
+
+    }
+}
