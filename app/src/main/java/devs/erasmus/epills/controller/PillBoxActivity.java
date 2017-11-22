465593ea443e7bb5ea35f31fb70ca7f34bbd6eb3
Jonas Ebel <jonas.ebel@tum.de>
2017-11-22 18:07:27 +0000

    Add Empty PillBoxActivity

---
+++ app/src/main/java/devs/erasmus/epills/controller/PillBoxActivity.java
@@ -0,0 +1,15 @@
+package devs.erasmus.epills.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import devs.erasmus.epills.R;
+
+public class PillBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill_box2);
+    }
+}
