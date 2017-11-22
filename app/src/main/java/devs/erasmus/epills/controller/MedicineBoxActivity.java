575f941795db3b2c9af521aba544b71a7c6c2191
Jonas Ebel <jonas.ebel@tum.de>
2017-11-22 18:58:20 +0000

    Creating Toolbar, RecyclerView

--- app/src/main/java/devs/erasmus/epills/controller/MedicineBoxActivity.java
+++ app/src/main/java/devs/erasmus/epills/controller/MedicineBoxActivity.java
@@ -2,14 +2,34 @@ package devs.erasmus.epills.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.materialdrawer.Drawer;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import devs.erasmus.epills.R;
+import devs.erasmus.epills.widget.NavigationDrawer;
 
 public class MedicineBoxActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    Drawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine_box);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        drawer = NavigationDrawer.getDrawerBuilder(this,this,toolbar).build();
+
+
     }
 }
